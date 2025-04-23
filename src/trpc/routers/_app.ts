4164c3a724d9927d4c9e9b48772d28d3052bfe8a
jasser <jasserj663@gmail.com>
2025-04-23 23:01:39 +0100

    completing trpc configuration

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,7 @@
 import { z } from 'zod';
-import { baseProcedure, createTRPCRouter } from '../init';
-import { TRPCError } from '@trpc/server';
+import { protectedProcedure, createTRPCRouter } from '../init';
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
+  hello:protectedProcedure 
     .input(
       
       z.object({
@@ -10,9 +9,8 @@ export const appRouter = createTRPCRouter({
       }),
     )
     .query((opts) => {
-      throw new TRPCError({ code: 'BAD_REQUEST' });
+      console.log({fromContext: opts.ctx.user});
       return {
-        
         greeting: `hello ${opts.input.text}`,
       };
     }),
